07a9e82534e51582e4d8a29717de34a77bffc417
Manjanka <masjaha88@gmail.com>
2018-04-25 17:01:05 +0300

    qoutes files

---
+++ script.js
@@ -0,0 +1,23 @@
+let JSONItems = [];
+let index = Math.floor(Math.random() * (43 + 1));
+
+$.get( "http://quotes.stormconsultancy.co.uk/quotes.json", (data)=>{
+  JSONItems = data;
+  $(".sentence").html('"' + JSONItems[index]["quote"] + '"');
+  $(".author").html(JSONItems[index]["author"]);
+});
+
+
+$(".btn-next").on("click",()=>{
+    index = Math.floor(Math.random() * (43 + 1));
+    $(".sentence").html('"' + JSONItems[index]["quote"] + '"');
+    $(".author").html(JSONItems[index]["author"]);
+    
+    let randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+    $(".wrapper, .btn").css('background',randomColor);
+});
+
+$(".tweet img").on("click",()=>{
+    let tweetUrl = 'https://twitter.com/share?text=' + '"' + JSONItems[index]["quote"] + '"';
+    window.open(tweetUrl)
+});
